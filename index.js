530f6f6d383a936c99b56f8f601a1a2b45c9a9a2
fartun <fartunwario34@gmail.com>
2023-03-25 09:53:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+console.log('adding event listener...'); 
+const input = document.getElementById('input');   
+function clickAlert() {
+alert('I was clicked!');
+
 }
+input.addEventListener('click', clickAlert);
+}
+
